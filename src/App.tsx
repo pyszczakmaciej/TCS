636fe09baeabf5af5dcbfa45ce3119f03b5f0ebb
Pyszczak <pyszczak.maciej@outlook.com>
2024-01-25 21:43:46 +0100

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import PrivateRoute from "./components/ProtectedRoute.component";
-import AuthPage from "./pages/Auth.page";
 import HomePage from "./pages/Home.page";
+import AuthPage from "./pages/auth/Auth.page";
 
 function App() {
   return (
-    <div>
+    <div className="wrapper">
       <Router>
         <Routes>
           <Route path="/auth" element={<AuthPage />} />
